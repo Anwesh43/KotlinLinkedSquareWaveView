41a94199c4e3587fb9671ac0f3ca86732ba5ab0e
Anwesh43 <anweshthecool0@gmail.com>
2018-07-04 08:56:29 +0530

    importing classes required and declaring number f nodes

--- linkedsquarewaveview/src/main/java/com/anwesh/uiprojects/linkedsquarewaveview/LinkedSquareWaveView.kt
+++ linkedsquarewaveview/src/main/java/com/anwesh/uiprojects/linkedsquarewaveview/LinkedSquareWaveView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.linkedsquarewaveview
 /**
  * Created by anweshmishra on 04/07/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val SWV_NODES : Int = 5
