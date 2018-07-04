b01f6ec0ad275e447887b65d8e213dd0475c54d4
Anwesh43 <anweshthecool0@gmail.com>
2018-07-04 08:58:21 +0530

    adding LinkedSquareWaveView class

--- linkedsquarewaveview/src/main/java/com/anwesh/uiprojects/linkedsquarewaveview/LinkedSquareWaveView.kt
+++ linkedsquarewaveview/src/main/java/com/anwesh/uiprojects/linkedsquarewaveview/LinkedSquareWaveView.kt
@@ -11,3 +11,23 @@ import android.graphics.Paint
 import android.graphics.Canvas
 
 val SWV_NODES : Int = 5
+
+class LinkedSquareWaveView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+
+}
